d9a6f69a072cc2769937895503b798c8bea44390
evandcoleman <evandcoleman@users.noreply.github.com>
2021-08-30 11:59:33 -0400

    fixes

--- Formula/scipio.rb
+++ Formula/scipio.rb
@@ -1,17 +1,17 @@
 class Scipio < Formula
-  desc "Scipio: A cache tool for Swift Package Manager"
+  desc "A cache tool for Swift Package Manager"
   homepage "https://github.com/evandcoleman/Scipio"
   url "https://github.com/evandcoleman/Scipio/archive/refs/tags/0.1.3.tar.gz"
   sha256 "bc116cde5522b44815ab4334c6106e959976409d9186b17803beb3acde2fc2b0"
-  head "https://github.com/evandcoleman/Scipio.git", :branch => "main"
   license "Apache-2.0"
+  head "https://github.com/evandcoleman/Scipio.git", branch: "main"
 
   def install
-    system "swift build --disable-sandbox -c release"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/scipio"
   end
 
   test do
-    system "#{bin}/scipio --version"
+    system "#{bin}/scipio", "--version"
   end
 end
