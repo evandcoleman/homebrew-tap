31099844a43f97c247eb952c7eed653d06c7ee12
evandcoleman <evandcoleman@users.noreply.github.com>
2021-08-30 15:50:51 +0000

    scipio 0.1.3

--- Formula/scipio.rb
+++ Formula/scipio.rb
@@ -1,8 +1,8 @@
 class Scipio < Formula
   desc "Scipio: A cache tool for Swift Package Manager"
   homepage "https://github.com/evandcoleman/Scipio"
-  url "https://github.com/evandcoleman/Scipio/archive/refs/tags/0.1.2.tar.gz"
-  sha256 "9fb11105e29db427a8d0414c07aa5a8a4571a4b72108d8d9dbd74f18618b7afa"
+  url "https://github.com/evandcoleman/Scipio/archive/refs/tags/0.1.3.tar.gz"
+  sha256 "bc116cde5522b44815ab4334c6106e959976409d9186b17803beb3acde2fc2b0"
   head "https://github.com/evandcoleman/Scipio.git", :branch => "main"
   license "Apache-2.0"
 
