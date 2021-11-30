95693957ae286006553a66f6dda36a57cb4a5dae
evandcoleman <evandcoleman@users.noreply.github.com>
2021-11-30 20:29:48 +0000

    scipio 0.2.4
    Closes #30.
    
    Signed-off-by: Evan Coleman <hello@edc.me>

--- Formula/scipio.rb
+++ Formula/scipio.rb
@@ -1,8 +1,8 @@
 class Scipio < Formula
   desc "Cache tool for Swift Package Manager"
   homepage "https://github.com/evandcoleman/Scipio"
-  url "https://github.com/evandcoleman/Scipio/archive/refs/tags/0.2.3.tar.gz"
-  sha256 "35892f03072b42919686ea46327c34ba175c4f5e07f441028fe21f11bd77878a"
+  url "https://github.com/evandcoleman/Scipio/archive/refs/tags/0.2.4.tar.gz"
+  sha256 "4eaf96cf545cd2b5d4905e65ca6aad8f44e1d923a9f177773f203d39f83598be"
   license "Apache-2.0"
   head "https://github.com/evandcoleman/Scipio.git", branch: "main"
 
