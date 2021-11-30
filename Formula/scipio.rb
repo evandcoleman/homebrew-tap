9f05adeb86bcf57fa4e49025a5501ee92ca46211
Evan Coleman <hello@edc.me>
2021-11-30 20:57:04 +0000

    scipio: update 0.2.4 bottle.

--- Formula/scipio.rb
+++ Formula/scipio.rb
@@ -7,9 +7,9 @@ class Scipio < Formula
   head "https://github.com/evandcoleman/Scipio.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/evandcoleman/homebrew-tap/releases/download/scipio-0.2.3"
-    sha256 cellar: :any_skip_relocation, big_sur:  "61a84fdf62c79a2dc52f9c96e6bc52853c1c29d5d7a98805f0395efdda5fb96d"
-    sha256 cellar: :any_skip_relocation, catalina: "e4bf25c6fb03e6dbcf4e05b512db0c5fa9e707b285b0ac0bac2a5bd085c505dd"
+    root_url "https://github.com/evandcoleman/homebrew-tap/releases/download/scipio-0.2.4"
+    sha256 cellar: :any_skip_relocation, big_sur:  "f2b8ec8c3a14dcf37ded3fd47bae2fbfa7ebfc34201bdbaf0e75d3c90217fada"
+    sha256 cellar: :any_skip_relocation, catalina: "af7312cb6a449fe2a6ffa5ed673494f34115f4516e81d9987bc276d0af61272d"
   end
 
   depends_on xcode: ["11.0", :build]
